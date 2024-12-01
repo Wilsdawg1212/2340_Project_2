5567f73ab956e8ddf719039c58810f7352ab879e
Wilsdawg1212 <145064244+Wilsdawg1212@users.noreply.github.com>
2024-12-01 18:41:01 -0500

    Trying to get down to one resource

--- Project_2_2340/settings.py
+++ Project_2_2340/settings.py
@@ -160,7 +160,7 @@ USE_TZ = True
 
 
 STATIC_URL = os.getenv('STATIC_URL', '/static/')
-STATICFILES_DIRS = [BASE_DIR / "static"]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
