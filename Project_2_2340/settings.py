f7b5574acd049eaf1018f8eaee1ddf1f66d360b8
Wilsdawg1212 <145064244+Wilsdawg1212@users.noreply.github.com>
2024-12-01 19:58:16 -0500

    Fixing static files paths

--- Project_2_2340/settings.py
+++ Project_2_2340/settings.py
@@ -162,6 +162,7 @@ USE_TZ = True
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+print(STATIC_ROOT)
 
 
 # Default primary key field type
